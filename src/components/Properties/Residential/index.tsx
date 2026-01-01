ccbd3e7b6f8660b2a432b09eda45c943204a9da4
ovara1mark <markovara1@gmail.com>
2026-01-01 14:42:10 +0000

    Add full Next.js project files are read

--- src/components/Properties/Residential/index.tsx
+++ src/components/Properties/Residential/index.tsx
@@ -1,15 +1,27 @@
-import PropertyCard from '@/components/Home/Properties/Card/Card'
-import { propertyHomes } from '@/app/api/propertyhomes'
+'use client'
 
-const ResidentialList: React.FC = () => {
+import { useEffect, useState } from 'react'
+import PropertyCard from '@/components/Home/Properties/Card/Card'
+
+const ResidentialList = () => {
+  const [homes, setHomes] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchHomes = async () => {
+      const res = await fetch('/api/propertyhomes')
+      const data = await res.json()
+      setHomes(data)
+    }
+
+    fetchHomes()
+  }, [])
+
   return (
     <section className='pt-0!'>
       <div className='container max-w-8xl mx-auto px-5 2xl:px-0'>
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10'>
-          {propertyHomes.slice(0, 3).map((item, index) => (
-            <div key={index} className=''>
-              <PropertyCard item={item} />
-            </div>
+          {homes.slice(0, 3).map((item, index) => (
+            <PropertyCard key={index} item={item} />
           ))}
         </div>
       </div>
